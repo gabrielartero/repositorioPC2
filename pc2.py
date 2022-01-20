9a05801fb39f714840fc913f8bbf825c055a1ec5
gabrielartero <96343420+gabrielartero@users.noreply.github.com>
2022-01-20 16:12:53 +0100

    Update pc2.py

--- pc2.py
+++ pc2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# REALIZADO POR GABRIEL ARTERO MONSALVATJE Y CARLOTA RUIZ DE CONEJO DE LA SEN
+
 import logging 
 import sys 
 from subprocess import call
@@ -9,31 +11,43 @@ import os
 logging.basicConfig(level=logging.DEBUG) 
 logger = logging.getLogger('pc2')
 
-def funcion1():
-    # COMANDOS NECESARIOS PARA LA PUESTA EN MARCHA DE LA APLICACIÓN	
-    call(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2.git"])
-    logger.debug("Repositorio clonado correctamente") 
 
-    call(["cd", "./practica_creativa2/bookinfo/src/productpage"])
+# GUARDAMOS EL VALOR DE LA VARIABLE DE ENTORNO
+numeroGrupo = str(os.environ.get('GROUP_NUMBER'))
 
+# COMANDOS NECESARIOS PARA LA PUESTA EN MARCHA DE LA APLICACIÓN	
+call(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2.git"])
+logger.debug("Repositorio clonado correctamente") 
 
-    call(["pip3", "install", "-r", "requirements.txt"])
-    logger.debug("El XML de C1 se ha creado correctamente") 
+# ACTUALIZAMOS EL SISTEMA
+call(["sudo", "apt-get", "update"])
+logger.debug("Sistema actualizado") 
 
+# INSTALAMOS PIP
+call(["sudo", "apt-get", "install", "python3-pip"])
+logger.debug("pip se ha instalado correctamente) 
 
-    # MODIFICAMOS EL TITULO DE LA APLICACION
-    call(["cd", "templates"])
-
-    ############ FALTA ESTA PARTE: HAY QUE USAR LA VARIABLE DE ENTORNO PREVIAMENTE CREADA PARA 
-    ############ MODIFICAR ESTE VALOR
-
-    fc1 = open("index.html", "w")
-    fc1.replace("{% block title %}Simple Bookstore App{% endblock %}", "{% block title %}Grupo 23{% endblock %}")
-    fc1.close()
+# AÑADIMOS LOS REQUIREMENTS NECESARIOS
+call(["pip3", "install", "-r", "practica_creativa2/bookinfo/src/productpage/requirements.txt"])
+logger.debug("Las dependencias han sido instaladas") 
 
 
-    call(["python3", "productpage-monolith.py", "9080"])
-    logger.debug("La aplicación se está ejecuntando correctamente. Puede acceder a ella desde https://<ip-publica>:9080/productpage") 
+# MODIFICAMOS EL TITULO DE LA APLICACION
+call(["cp", "/practica_creativa2/bookinfo/src/productpage/templates/productpage.html", "/practica_creativa2/bookinfo/src/productpage/templates/productpageCopia.html"])
+fOriginal = open("practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'r')
+fCopia = open("practica_creativa2/bookinfo/src/productpage/templates/productpageCopia.html", 'r')
+for line in fCopia:
+    if "{% block title %}Simple Bookstore App{% endblock %}" in line:
+        fOriginal.write("{% block title %}Simple Bookstore App - GRUPO" + numeroGrupo + "{% endblock %}")
+    else:
+        fOriginal.write(line)
+fOriginal.close()
+fCopia.close()
+logger.debug("Se ha actualizado el título de la aplicación") 
 
+
+# ARRANCAMOS LA APLICACIÓN
+call(["python3", "productpage-monolith.py", "9080"])
+logger.debug("La aplicación se está ejecuntando correctamente. Puede acceder a ella desde https://<ip-publica>:9080/productpage") 
 
 
